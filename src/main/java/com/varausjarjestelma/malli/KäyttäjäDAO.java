8a19d0831456491db8b70c4ea87c90937dfc506c
Veeti Ahlstén <veeti.b.ahlsten@gmail.com>
2020-10-20 16:10:34 +0300

    KäyttäjäDAO ja VarauksetDAO luotu.

---
+++ src/main/java/com/varausjarjestelma/malli/KäyttäjäDAO.java
@@ -0,0 +1,192 @@
+package com.varausjarjestelma.malli;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class KäyttäjäDAO {
+	
+	private SessionFactory istuntotehdas;
+	private StandardServiceRegistry rekisteri;
+
+	public KäyttäjäDAO() {
+		
+		istuntotehdas = null;
+		rekisteri = null;
+		
+		try {
+			istuntotehdas = new Configuration().configure().buildSessionFactory();
+			rekisteri = new StandardServiceRegistryBuilder().configure().build();
+		} catch (Exception e) {
+			System.out.println("TilaDAO - istuntotehtaan luonti epäonnistui");
+			istuntotehdas.close();
+			StandardServiceRegistryBuilder.destroy(rekisteri);
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	public Käyttäjä[] haeKaikkiKayttajat() {
+		Session istunto = null;
+		Transaction transaktio = null;
+		Käyttäjä[] palautus = null;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+
+			@SuppressWarnings("unchecked")
+			List<Käyttäjä> käyttäjät = istunto.createQuery("from Varaukset").getResultList();
+			palautus = new Käyttäjä[käyttäjät.size()];
+
+			istunto.getTransaction().commit();
+			käyttäjät.toArray(palautus);
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			System.err.println(e.getMessage());
+
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	public boolean lisaaKayttaja(Käyttäjä käyttäjä) {
+		Transaction transaktio = null;
+		Session istunto = null;
+		boolean palautus = false;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+
+			istunto.save(käyttäjä);
+			transaktio.commit();
+			
+			palautus = true;
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			e.printStackTrace();
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	public Käyttäjä etsiKayttaja(int id) {
+		Käyttäjä palautus = null;
+		Session istunto = null;
+		Transaction transaktio = null;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+
+			palautus = (Käyttäjä) istunto.createQuery("from Kayttaja where id = " + id).getSingleResult();
+
+			istunto.getTransaction().commit();
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			System.err.println(e.getMessage());
+
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	public boolean muokkaaKayttaja(int id, Käyttäjä käyttäjä) {
+		Session istunto = null;
+		Transaction transaktio = null;
+		boolean palautus = false;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+			Käyttäjä muokattava = etsiKayttaja(id);
+
+			muokattava.setEtunimi(käyttäjä.getEtunimi());
+			muokattava.setSukunimi(käyttäjä.getSukunimi());
+			muokattava.setSposti(käyttäjä.getSposti());
+
+			istunto.update(muokattava);
+			transaktio.commit();
+			
+			palautus = true;
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			System.out.println("Tilan muokkaus epäonnistui");
+			e.printStackTrace();
+
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	// TODO
+	public boolean poistaKayttaja(Käyttäjä käyttäjä) {
+		Session istunto = null;
+		Transaction transaktio = null;
+		boolean palautus = false;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+
+			istunto.delete(käyttäjä);
+			transaktio.commit();
+			
+			palautus = true;
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			System.err.println(e.getMessage());
+
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		
+		if (istuntotehdas != null)
+			istuntotehdas.close();
+		
+		if (rekisteri != null)
+			StandardServiceRegistryBuilder.destroy(rekisteri);
+	}
+
+}
---
+++ src/main/java/com/varausjarjestelma/malli/VarauksetDAO.java
@@ -0,0 +1,191 @@
+package com.varausjarjestelma.malli;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class VarauksetDAO {
+	
+	private SessionFactory istuntotehdas;
+	private StandardServiceRegistry rekisteri;
+
+	public VarauksetDAO() {
+		
+		istuntotehdas = null;
+		rekisteri = null;
+		
+		try {
+			istuntotehdas = new Configuration().configure().buildSessionFactory();
+			rekisteri = new StandardServiceRegistryBuilder().configure().build();
+		} catch (Exception e) {
+			System.out.println("TilaDAO - istuntotehtaan luonti epäonnistui");
+			istuntotehdas.close();
+			StandardServiceRegistryBuilder.destroy(rekisteri);
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	public Varaukset[] haeKaikkiVaraukset() {
+		Session istunto = null;
+		Transaction transaktio = null;
+		Varaukset[] palautus = null;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+
+			@SuppressWarnings("unchecked")
+			List<Varaukset> varaukset = istunto.createQuery("from Varaukset").getResultList();
+			palautus = new Varaukset[varaukset.size()];
+
+			istunto.getTransaction().commit();
+			varaukset.toArray(palautus);
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			System.err.println(e.getMessage());
+
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	public boolean lisaaVaraus(Varaukset varaus) {
+		Transaction transaktio = null;
+		Session istunto = null;
+		boolean palautus = false;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+
+			istunto.save(varaus);
+			transaktio.commit();
+			
+			palautus = true;
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			e.printStackTrace();
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	public Varaukset etsiVaraus(int id) {
+		Varaukset palautus = null;
+		Session istunto = null;
+		Transaction transaktio = null;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+
+			palautus = (Varaukset) istunto.createQuery("from Varaukset where id = " + id).getSingleResult();
+
+			istunto.getTransaction().commit();
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			System.err.println(e.getMessage());
+
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	public boolean muokkaaVarausta(int id, Varaukset varaus) {
+		Session istunto = null;
+		Transaction transaktio = null;
+		boolean palautus = false;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+			Varaukset muokattava = etsiVaraus(id);
+
+			muokattava.setAlkuAika(varaus.getAlkuAika());
+			muokattava.setLoppuAika(varaus.getLoppuAika());
+
+			istunto.update(muokattava);
+			transaktio.commit();
+			
+			palautus = true;
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			System.out.println("Tilan muokkaus epäonnistui");
+			e.printStackTrace();
+
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	// TODO
+	public boolean poistaVaraus(Varaukset varaus) {
+		Session istunto = null;
+		Transaction transaktio = null;
+		boolean palautus = false;
+
+		// try-with-resources ei ole tarjolla. JRE-versio-ongelma.
+		try {
+			istunto = istuntotehdas.openSession();
+			transaktio = istunto.beginTransaction();
+
+			istunto.delete(varaus);
+			transaktio.commit();
+			
+			palautus = true;
+
+		} catch (Exception e) {
+			if (transaktio != null)
+				transaktio.rollback();
+			System.err.println(e.getMessage());
+
+		} finally {
+			if (istunto != null)
+				istunto.close();
+		}
+
+		return palautus;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		
+		if (istuntotehdas != null)
+			istuntotehdas.close();
+		
+		if (rekisteri != null)
+			StandardServiceRegistryBuilder.destroy(rekisteri);
+	}
+
+}
