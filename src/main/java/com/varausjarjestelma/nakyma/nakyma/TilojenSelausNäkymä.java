b7f62fb847bb7558416ba4b3033daf6339703eb1
Sebastian Sarviala <sebastian.sarviala@metropolia.fi>
2020-11-04 01:12:52 +0200

    GridPane BorderPanen keskiosassa on nyt korvattu TilePanella

--- src/main/java/com/varausjarjestelma/nakyma/nakyma/TilojenSelausNäkymä.java
+++ src/main/java/com/varausjarjestelma/nakyma/nakyma/TilojenSelausNäkymä.java
@@ -1,5 +1,8 @@
 package com.varausjarjestelma.nakyma.nakyma;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.varausjarjestelma.kontrolleri.TilojenSelausKontrolleri;
 import com.varausjarjestelma.malli.Tila;
 
@@ -13,43 +16,71 @@ import javafx.scene.text.Text;
 public class TilojenSelausNäkymä extends BorderPane {
 
 	private final TilojenSelausKontrolleri kontrolleri;
+	private final List<Pane> tilaPanet;
 
 	public TilojenSelausNäkymä() {
 		kontrolleri = new TilojenSelausKontrolleri(this);
+		tilaPanet = new ArrayList<Pane>();
 
-		int col = 0, row = 0;
-		GridPane grid = new GridPane();
+		final TilePane bg = new TilePane();
 		Tila[] tilat = kontrolleri.haeTilat();
-		
+
+		bg.setOnMouseClicked(new EventHandler<Event>() {
+
+			public void handle(Event event) {
+				if (event.getTarget().equals(bg)){
+					setRight(null);
+					poistaKorostus();
+				}
+			}
+		});
+
 		for (final Tila tila : tilat) {
-			GridPane tilaPane = new GridPane();
+			final GridPane tilaPane = new GridPane();
 			Text nimi = new Text(tila.getNimi());
 			Text kaupunki = new Text(tila.getKaupunki());
 			Text hlömäärä = new Text("Henkilömäärä: " + tila.getHlomaara());
+			tilaPanet.add(tilaPane);
 			tilaPane.add(nimi, 0, 0);
 			tilaPane.add(kaupunki, 0, 1);
 			tilaPane.add(hlömäärä, 0, 2);
 			tilaPane.setOnMouseClicked(new EventHandler<Event>() {
 
 				public void handle(Event event) {
-					System.out.println("Klikatun tilan ID = " + tila.getID());
+					poistaKorostus();
+					tilaPane.setBackground(new Background(new BackgroundFill(Color.GOLD, null, null)));
+
+					GridPane tiedot = new GridPane();
+					String[] otsikot = { "Nimi", "Kaupunki", "Osoite", "Henkilömäärä", "Kuvaus" };
+					String[] arvot = { tila.getNimi(), tila.getKaupunki(), tila.getOsoite(), "" + tila.getHlomaara(),
+							tila.getKuvaus() };
+					int row = 0;
+					for (int i = 0; i < otsikot.length && i < arvot.length; i++) {
+						String arvo = arvot[i];
+						if (arvo != null && !arvo.isEmpty()) {
+							tiedot.add(new Text(otsikot[i] + ":"), 0, row);
+							tiedot.add(new Text(arvo), 1, row++);
+						}
+					}
+					setRight(tiedot);
 				}
 
 			});
-			grid.add(tilaPane, col, row);
-			if (++col > 1) {
-				row++;
-				col = 0;
-			}
+			bg.getChildren().add(tilaPane);
 			tilaPane.setPadding(new Insets(5));
 			tilaPane.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
 		}
 
-		setCenter(grid);
-		grid.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, null, null)));
-		grid.setHgap(15);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(20));
+		setCenter(bg);
+		bg.setBackground(new Background(new BackgroundFill(Color.DARKOLIVEGREEN, null, null)));
+		bg.setHgap(15);
+		bg.setVgap(10);
+		bg.setPadding(new Insets(20));
+	}
+	
+	private void poistaKorostus() {
+		for (Pane pane : tilaPanet)
+			pane.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
 	}
 
 }
