11cf76cfe263bab6146aa03cfa30be0d414fbe91
Veeti Ahlstén <veeti.b.ahlsten@gmail.com>
2020-11-10 14:04:09 +0200

    Kalenterit on nyt näkyvissä omassa luokassaan.

--- src/main/java/com/varausjarjestelma/käyttöliittymä/tilojenselaus/Kalenteri.java
+++ src/main/java/com/varausjarjestelma/käyttöliittymä/tilojenselaus/Kalenteri.java
@@ -7,12 +7,14 @@ import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Locale;
 
+import com.sun.javafx.scene.control.skin.DatePickerSkin;
 import com.varausjarjestelma.kontrolleri.Kontrolleri;
 import com.varausjarjestelma.käyttöliittymä.TempMain;
 import com.varausjarjestelma.malli.Varaukset;
 
 import javafx.application.Application;
 import javafx.geometry.HPos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
@@ -57,26 +59,6 @@ public class Kalenteri extends TempMain{
         checkOutDatePicker = new DatePicker();
         checkInDatePicker.setValue(LocalDate.now());
         
-        // Rajaa minä päivinä on varattavissa | Ei voi varata jos päivä on jo mennyt
-        final Callback<DatePicker, DateCell> dayCellFactory = 
-            new Callback<DatePicker, DateCell>() {
-                public DateCell call(final DatePicker datePicker) {
-                    return new DateCell() {
-                        @Override
-                        public void updateItem(LocalDate item, boolean empty) {
-                            super.updateItem(item, empty);
-                           
-                            if (item.isBefore(
-                                    checkInDatePicker.getValue().plusDays(1))
-                                ) {
-                                    setDisable(true);
-                                    setStyle("-fx-background-color: #ffc0cb;");
-                            }
-                    }
-                };
-            }
-        };
-        
         
      // Rajaa minä päivinä on varattavissa | Ei voi varata jos päivä on jo varattu
         final Callback<DatePicker, DateCell> dayCellFactoryVaraukset = 
@@ -86,6 +68,13 @@ public class Kalenteri extends TempMain{
                             @Override
                             public void updateItem(LocalDate item, boolean empty) {
                                 super.updateItem(item, empty);
+                                
+                                if (item.isBefore(
+                                        checkInDatePicker.getValue().plusDays(1))
+                                    ) {
+                                        setDisable(true);
+                                        //setStyle("-fx-background-color: #fffff;");
+                                }
                                 
                                 for(int i = 0; i < varaukset.length; i++) {
                                 	ajat[i] = (varaukset[i].getAlkuAika()).toLocalDateTime().toLocalDate();
@@ -103,8 +92,14 @@ public class Kalenteri extends TempMain{
             };
         
         checkInDatePicker.setDayCellFactory(dayCellFactoryVaraukset);
-        checkOutDatePicker.setDayCellFactory(dayCellFactory);
+        checkOutDatePicker.setDayCellFactory(dayCellFactoryVaraukset);
         checkOutDatePicker.setValue(checkInDatePicker.getValue().plusDays(1));
+        
+        DatePickerSkin datePickerSkinIn = new DatePickerSkin(checkInDatePicker);
+        Node popupContentIn = datePickerSkinIn.getPopupContent();
+        
+        DatePickerSkin datePickerSkinOut = new DatePickerSkin(checkOutDatePicker);
+        Node popupContentOut = datePickerSkinOut.getPopupContent();
         
         GridPane gridPane = new GridPane();
         gridPane.setHgap(10);
@@ -112,11 +107,11 @@ public class Kalenteri extends TempMain{
         Label checkInlabel = new Label("Check-In Date:");
         gridPane.add(checkInlabel, 0, 0);
         GridPane.setHalignment(checkInlabel, HPos.LEFT);
-        gridPane.add(checkInDatePicker, 0, 1);
+        gridPane.add(popupContentIn, 0, 1);
         Label checkOutlabel = new Label("Check-Out Date:");
         gridPane.add(checkOutlabel, 0, 2);
         GridPane.setHalignment(checkOutlabel, HPos.LEFT);
-        gridPane.add(checkOutDatePicker, 0, 3);
+        gridPane.add(popupContentOut, 0, 3);
         
         vbox.getChildren().add(gridPane);
     }
