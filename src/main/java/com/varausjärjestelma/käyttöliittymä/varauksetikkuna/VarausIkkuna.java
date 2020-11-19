741525a11f08859d53a6ffdea0f2eefb4d6a991e
Veeti Ahlstén <veeti.b.ahlsten@gmail.com>
2020-11-19 16:28:06 +0200

    Varaus on nyt mahdollista poistaa.

--- src/main/java/com/varausjärjestelma/käyttöliittymä/varauksetikkuna/VarausIkkuna.java
+++ src/main/java/com/varausjärjestelma/käyttöliittymä/varauksetikkuna/VarausIkkuna.java
@@ -10,6 +10,14 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
 import com.varausjarjestelma.kontrolleri.*;
 import com.varausjarjestelma.malli.Varaukset;
 
@@ -27,6 +35,8 @@ public class VarausIkkuna extends BorderPane {
         TilePane pane = new TilePane();
         pane.setHgap(10);
         pane.setVgap(10);
+        
+        varaukset = sort(varaukset);
 		
 		for(final Varaukset v: varaukset) {
 		    Button btn = new Button(v.getTila().getNimi());
@@ -44,6 +54,31 @@ public class VarausIkkuna extends BorderPane {
         
         root.getChildren().add(pane);
         
+	}
+	
+	private Varaukset[] sort(Varaukset[] varaukset) {
+		ArrayList<Date> list = new ArrayList<Date>();
+		
+		for(Varaukset v : varaukset) {
+			list.add(v.getAlkuAika());
+		}
+		
+		Collections.sort(list);
+		
+		System.out.println(list);
+		
+		Varaukset[] sorted = new Varaukset[varaukset.length];
+		
+		for(Date d : list) {
+			for(int i = 0; i < varaukset.length; i++) {
+				if(varaukset[i].getAlkuAika() == d) {
+					sorted[i] = varaukset[i];
+					//System.out.println(sorted[i].getAlkuAika());
+				}
+			}
+		}
+		
+		return sorted;
 	}
 	
 	public StackPane getRoot() {
--- src/main/java/com/varausjärjestelma/käyttöliittymä/varauksetikkuna/VarausPopUp.java
+++ src/main/java/com/varausjärjestelma/käyttöliittymä/varauksetikkuna/VarausPopUp.java
@@ -1,5 +1,7 @@
 package com.varausjärjestelma.käyttöliittymä.varauksetikkuna;
 
+import java.text.SimpleDateFormat;
+
 import com.varausjarjestelma.kontrolleri.Kontrolleri;
 import com.varausjarjestelma.malli.Tila;
 import com.varausjarjestelma.malli.Varaukset;
@@ -16,10 +18,6 @@ import javafx.stage.*;
 
 public class VarausPopUp {
 	private Kontrolleri kontrolleri;
-	private Varaukset varaus;
-	private String[] otsikot = { "Nimi", "Kaupunki", "Osoite",
-			"Henkilömäärä", "Kuvaus", "Alkupäivämäärä",
-			"Loppupäivämäärä", "Alkukellonaika", "Loppukellonaika" };
 	
 	public VarausPopUp(Kontrolleri k) {
 		kontrolleri = k;
@@ -36,19 +34,31 @@ public class VarausPopUp {
 			Label labelOsoite = new Label("Tilan osoite: " + v.getTila().getOsoite());
 			Label labelKuvaus = new Label("Tilan kuvaus: " + v.getTila().getKuvaus());
 			
-			Label labelAlku = new Label("Varauksen alku: " + v.getAlkuAika());
-			Label labelLoppu = new Label("Varauksen loppu: " + v.getLoppuAika());
-			      
-			     
-			Button button1= new Button("Close this pop up window");     
-			button1.setOnAction(new EventHandler<ActionEvent>() {
+			Label labelAlku = new Label("Varauksen alku: " + new SimpleDateFormat("dd-MM-yyyy HH:mm").format(v.getAlkuAika()));
+			Label labelLoppu = new Label("Varauksen loppu: " + new SimpleDateFormat("dd-MM-yyyy HH:mm").format(v.getLoppuAika()));
+			
+			Button close = new Button("Close"); 
+			close.setMaxWidth(100);
+			close.setOnAction(new EventHandler<ActionEvent>() {
 				public void handle(ActionEvent e) {
 					popupwindow.close();
 				}
 			});
 			
+			Button delete = new Button("Delete");
+			delete.setStyle("-fx-background-color: red; -fx-text-fill: white;");
+			delete.setMaxWidth(150);
+			delete.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent e) {
+					kontrolleri.poistaVaraus(v.getID());
+					popupwindow.close();
+				}
+			});
+			
 			VBox layout= new VBox(10);
-			layout.getChildren().addAll(labelNimi, labelOsoite, labelKuvaus, labelAlku, labelLoppu, button1);
+			layout.getChildren().addAll(labelNimi, labelOsoite, labelKuvaus, labelAlku, labelLoppu, close);
+			layout.setAlignment(Pos.CENTER);
+			layout.getChildren().add(delete);
 			layout.setAlignment(Pos.CENTER);
 			Scene scene1= new Scene(layout, 300, 250); 
 			popupwindow.setScene(scene1);
