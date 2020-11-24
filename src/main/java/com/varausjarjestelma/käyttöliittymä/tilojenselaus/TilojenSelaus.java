e0c284d01d05a12e0b1657d9f85ff3dc4379beb3
Sebastian Sarviala <sebastian.sarviala@metropolia.fi>
2020-11-24 16:35:55 +0200

    javadoc-kommentit lisätty luokkaan TilojenSelaus

--- src/main/java/com/varausjarjestelma/käyttöliittymä/tilojenselaus/TilojenSelaus.java
+++ src/main/java/com/varausjarjestelma/käyttöliittymä/tilojenselaus/TilojenSelaus.java
@@ -19,8 +19,8 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 /**
- * An extension to border pane, which serves as a
- * window for browsing and reserving spaces. 
+ * An extension to border pane, which serves as a window for browsing and
+ * reserving spaces.
  * 
  * @author E. Niemi, S. Sarviala
  *
@@ -31,17 +31,19 @@ public class TilojenSelaus extends BorderPane {
 	private final List<Pane> tilaPanet;
 	private GridPane korostettu;
 
+	/**
+	 * Constructor. Fills this border pane with desired content.
+	 */
 	public TilojenSelaus() {
 		kontrolleri = new TilojenSelausKontrolleri(this);
 		tilaPanet = new ArrayList<Pane>();
 		korostettu = null;
 		final TilePane tausta = new TilePane();
 		Tila[] tilat = kontrolleri.haeTilat();
-		
-		
+
 		HBox hbox = new HBox();
 		hbox.setPadding(new Insets(5, 5, 5, 5));
-        hbox.setSpacing(5);
+		hbox.setSpacing(5);
 		Button buttonEnglish = I18n.buttonForKey("button.english");
 		buttonEnglish.setTooltip(I18n.tooltipForKey("button.english.tooltip"));
 		buttonEnglish.setOnAction((evt) -> switchLanguage(Locale.ENGLISH));
@@ -51,7 +53,6 @@ public class TilojenSelaus extends BorderPane {
 		buttonFinnish.setOnAction((evt) -> switchLanguage(I18n.FINNISH));
 		hbox.getChildren().add(buttonFinnish);
 		setTop(hbox);
-
 
 		tausta.setOnMouseClicked(new EventHandler<Event>() {
 
@@ -68,13 +69,13 @@ public class TilojenSelaus extends BorderPane {
 			final GridPane tilaPaneeli = new GridPane();
 
 			Text tilannimi = I18n.stringForText("tilannimi");
-			Text nimi = new Text(" " +tila.getNimi());
+			Text nimi = new Text(" " + tila.getNimi());
 			Text tilankaupunki = I18n.stringForText("tilankaupunki");
-			Text kaupunki = new Text(" " +tila.getKaupunki());
+			Text kaupunki = new Text(" " + tila.getKaupunki());
 			Text henkilömäärä = I18n.stringForText("henkilötmäärä");
 			Text hlömäärä = new Text(" " + tila.getHlomaara());
 			tilaPanet.add(tilaPaneeli);
-			
+
 			tilaPaneeli.add(tilannimi, 0, 0);
 			tilaPaneeli.add(tilankaupunki, 0, 1);
 			tilaPaneeli.add(henkilömäärä, 0, 2);
@@ -89,14 +90,15 @@ public class TilojenSelaus extends BorderPane {
 
 					ScrollPane selausikkuna = new ScrollPane();
 					VBox tietoikkunanSisältö = new VBox();
-					Text[] otsikot = { I18n.stringForText("nimi"), I18n.stringForText("kaupunki"), I18n.stringForText("osoite"),
-							I18n.stringForText("henkilömäärä"), I18n.stringForText("kuvaus"), I18n.stringForText("ominaisuudet") };
+					Text[] otsikot = { I18n.stringForText("nimi"), I18n.stringForText("kaupunki"),
+							I18n.stringForText("osoite"), I18n.stringForText("henkilömäärä"),
+							I18n.stringForText("kuvaus"), I18n.stringForText("ominaisuudet") };
 					String[] arvot = { tila.getNimi(), tila.getKaupunki(), tila.getOsoite(), "" + tila.getHlomaara(),
 							tila.getKuvaus(), kontrolleri.näytäTilanOminaisuudetStringinä(tila.getID()) };
-					
+
 					tietoikkunanSisältö.setPadding(new Insets(0, 15, 0, 15));
 					tietoikkunanSisältö.setSpacing(10);
-					
+
 					for (int i = 0; i < otsikot.length && i < arvot.length; i++) {
 						VBox tiedonKappale = new VBox();
 						Text otsikko = otsikot[i];
@@ -109,17 +111,15 @@ public class TilojenSelaus extends BorderPane {
 							tietoikkunanSisältö.getChildren().add(tiedonKappale);
 						}
 					}
-					
+
 					selausikkuna.setContent(tietoikkunanSisältö);
 					selausikkuna.setMinWidth(225);
 					selausikkuna.setMaxWidth(225);
 
 					setRight(selausikkuna);
 
-					
-
 					// StackPane n = new StackPane();
-					Pane varauslomake = new TilojenSelausVarauslomake().getKokoVarausLomake(tila);					
+					Pane varauslomake = new TilojenSelausVarauslomake().getKokoVarausLomake(tila);
 
 					varauslomake.setPadding(new Insets(30));
 
@@ -140,6 +140,13 @@ public class TilojenSelaus extends BorderPane {
 		tausta.setPadding(new Insets(20));
 	}
 
+	/**
+	 * Adds a golden color to the given grid pane. This is meant to communicate to
+	 * the user which grid pane representing a space is selected. The golden color
+	 * is removed from any previously selected grid pane.
+	 * 
+	 * @param korostettava
+	 */
 	private void korosta(GridPane korostettava) {
 		poistaKorostus();
 
@@ -149,16 +156,18 @@ public class TilojenSelaus extends BorderPane {
 		korostettu = korostettava;
 	}
 
+	/**
+	 * Deselects the selected grid pane and removes the golden color from it.
+	 */
 	private void poistaKorostus() {
 		if (korostettu != null) {
 			korostettu.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
 			korostettu = null;
 		}
 	}
-	
-    private void switchLanguage(Locale locale) {
-        I18n.setLocale(locale);
-    }
 
+	private void switchLanguage(Locale locale) {
+		I18n.setLocale(locale);
+	}
 
 }
