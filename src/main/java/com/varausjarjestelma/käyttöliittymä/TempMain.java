18917e1ea3b8c6c549151e16b81ed9d2273f2765
eemelitn <eemeli.niemi@gmail.com>
2020-11-26 14:33:20 +0200

    tabit lisätty

--- src/main/java/com/varausjarjestelma/käyttöliittymä/TempMain.java
+++ src/main/java/com/varausjarjestelma/käyttöliittymä/TempMain.java
@@ -1,9 +1,19 @@
 package com.varausjarjestelma.käyttöliittymä;
 
+import java.util.Locale;
+
+import com.varausjarjestelma.i18n.I18n;
 import com.varausjarjestelma.käyttöliittymä.tilojenselaus.TilojenSelaus;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class TempMain extends Application {
@@ -12,7 +22,20 @@ public class TempMain extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setScene(new Scene(new TilojenSelaus(), 1000, 600));
+		
+		BorderPane mainPane = new BorderPane();
+
+		TabPane tabPane = new TabPane();
+        Tab tab1 = new Tab("Tilat"  , new TilojenSelaus());
+        tab1.closableProperty().setValue(false);
+        Tab tab2 = new Tab("Lisää uusi tila", new Label("Diibadaabadaa.."));
+        tabPane.getTabs().add(tab1);
+        tabPane.getTabs().add(tab2);
+        
+        mainPane.setTop(new TilojenSelaus().kieliboxi());
+		mainPane.setCenter(tabPane);
+        
+		primaryStage.setScene(new Scene(mainPane, 1000, 600));
 		primaryStage.show();
 		this.primaryStage = primaryStage;
 	}
--- src/main/java/com/varausjarjestelma/käyttöliittymä/tilojenselaus/TilojenSelaus.java
+++ src/main/java/com/varausjarjestelma/käyttöliittymä/tilojenselaus/TilojenSelaus.java
@@ -11,7 +11,10 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -41,7 +44,7 @@ public class TilojenSelaus extends BorderPane {
 		final TilePane tausta = new TilePane();
 		Tila[] tilat = kontrolleri.haeTilat();
 
-		HBox hbox = new HBox();
+	/*	HBox hbox = new HBox();
 		hbox.setPadding(new Insets(5, 5, 5, 5));
 		hbox.setSpacing(5);
 		Button buttonEnglish = I18n.buttonForKey("button.english");
@@ -53,6 +56,8 @@ public class TilojenSelaus extends BorderPane {
 		buttonFinnish.setOnAction((evt) -> switchLanguage(I18n.FINNISH));
 		hbox.getChildren().add(buttonFinnish);
 		setTop(hbox);
+		
+		*/
 
 		tausta.setOnMouseClicked(new EventHandler<Event>() {
 
@@ -138,6 +143,9 @@ public class TilojenSelaus extends BorderPane {
 		tausta.setHgap(15);
 		tausta.setVgap(10);
 		tausta.setPadding(new Insets(20));
+		
+		
+        
 	}
 
 	/**
@@ -168,6 +176,21 @@ public class TilojenSelaus extends BorderPane {
 
 	private void switchLanguage(Locale locale) {
 		I18n.setLocale(locale);
+	}
+	
+	public HBox kieliboxi() {
+		HBox hbox = new HBox();
+		hbox.setPadding(new Insets(5, 5, 5, 5));
+		hbox.setSpacing(5);
+		Button buttonEnglish = I18n.buttonForKey("button.english");
+		buttonEnglish.setTooltip(I18n.tooltipForKey("button.english.tooltip"));
+		buttonEnglish.setOnAction((evt) -> switchLanguage(Locale.ENGLISH));
+		hbox.getChildren().add(buttonEnglish);
+		Button buttonFinnish = I18n.buttonForKey("button.finnish");
+		buttonFinnish.setTooltip(I18n.tooltipForKey("button.finnish.tooltip"));
+		buttonFinnish.setOnAction((evt) -> switchLanguage(I18n.FINNISH));
+		hbox.getChildren().add(buttonFinnish);
+		return hbox;
 	}
 
 }
