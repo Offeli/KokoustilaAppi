67f2395f2abaa248ed81b1d48ca03b1f6eccc56e
Offeli <oskari.narhi@outlook.com>
2020-11-26 16:05:16 +0200

    VarausIkkunalle lisätty tabi

--- src/main/java/com/varausjarjestelma/käyttöliittymä/TempMain.java
+++ src/main/java/com/varausjarjestelma/käyttöliittymä/TempMain.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import com.varausjarjestelma.i18n.I18n;
 import com.varausjarjestelma.käyttöliittymä.tilojenselaus.TilojenSelaus;
+import com.varausjarjestelma.käyttöliittymä.varauksetikkuna.VarausIkkuna;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -14,34 +15,47 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class TempMain extends Application {
-	
+
 	private Stage primaryStage;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		BorderPane mainPane = new BorderPane();
 
 		TabPane tabPane = new TabPane();
-        Tab tab1 = new Tab("Tilat"  , new TilojenSelaus());
-        tab1.closableProperty().setValue(false);
-        Tab tab2 = new Tab("Lisää uusi tila", new Label("Diibadaabadaa.."));
-        tabPane.getTabs().add(tab1);
-        tabPane.getTabs().add(tab2);
-        
-        mainPane.setTop(new TilojenSelaus().kieliboxi());
+
+		TilojenSelaus tilojenselaus = new TilojenSelaus();
+		StackPane varausikkuna = new VarausIkkuna().getRoot();
+
+		Tab tab1 = new Tab();
+		tab1.setContent(tilojenselaus);
+		tab1.setText("Tilat");
+		tab1.closableProperty().setValue(false);
+		Tab tab2 = new Tab();
+		tab2.setContent(varausikkuna);
+		tab2.setText("Varaukset");
+		tab2.closableProperty().setValue(false);
+		Tab tab3 = new Tab();
+		tab3.setContent(new Label("Diibadaabadaa.."));
+		tab3.setText("Lisää uusi tila");
+		
+		tabPane.getTabs().addAll(tab1, tab2, tab3);
+
+		mainPane.setTop(new TilojenSelaus().kieliboxi());
 		mainPane.setCenter(tabPane);
-        
+
 		primaryStage.setScene(new Scene(mainPane, 1000, 600));
 		primaryStage.show();
 		this.primaryStage = primaryStage;
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 }
