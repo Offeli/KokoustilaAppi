bd651a4dd0d04995c2eb1725a6eb114b56bad938
Sebastian Sarviala <sebastian.sarviala@metropolia.fi>
2020-12-04 15:25:41 +0200

    oliomuuttuja primaryStage ja turhat importit poistettu

--- src/main/java/com/varausjarjestelma/käyttöliittymä/TempMain.java
+++ src/main/java/com/varausjarjestelma/käyttöliittymä/TempMain.java
@@ -1,8 +1,5 @@
 package com.varausjarjestelma.käyttöliittymä;
 
-import java.util.Locale;
-
-import com.varausjarjestelma.i18n.I18n;
 import com.varausjarjestelma.käyttöliittymä.tilanlisäys.TilanLisäyslomake;
 import com.varausjarjestelma.käyttöliittymä.tilojenselaus.TilojenSelaus;
 import com.varausjarjestelma.käyttöliittymä.varauksetikkuna.VarausIkkuna;
@@ -10,21 +7,14 @@ import com.varausjarjestelma.käyttöliittymä.varauksetikkuna.VarausIkkuna;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class TempMain extends Application {
-
-	private Stage primaryStage;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -48,27 +38,22 @@ public class TempMain extends Application {
 		Tab tab3 = new Tab();
 		tab3.setContent(tilanlisäys);
 		tab3.setText("Lisää uusi tila");
-		
+
 		tabPane.getTabs().addAll(tab1, tab2, tab3);
 
-
-
-        tabPane.getSelectionModel().selectedItemProperty().addListener(
-         new ChangeListener<Tab>() {
-        @Override
-        public void changed(ObservableValue<? extends Tab> ov, Tab t, Tab t1) {
-            System.out.println("Tabi vaihdettu");
-            tab2.setContent(new VarausIkkuna().getRoot());
-        }
-       }
-      );
+		tabPane.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Tab>() {
+			@Override
+			public void changed(ObservableValue<? extends Tab> ov, Tab t, Tab t1) {
+				System.out.println("Tabi vaihdettu");
+				tab2.setContent(new VarausIkkuna().getRoot());
+			}
+		});
 
 		mainPane.setTop(new TilojenSelaus().kieliboxi());
 		mainPane.setCenter(tabPane);
 
 		primaryStage.setScene(new Scene(mainPane, 1000, 600));
 		primaryStage.show();
-		this.primaryStage = primaryStage;
 	}
 
 	public static void main(String[] args) {
