1dbf8487163d4de8c1ec3a87c5c0c22692421c22
Sebastian Sarviala <sebastian.sarviala@metropolia.fi>
2020-12-10 15:32:33 +0200

    Javadoc-kommentit luokalle Ominaisuus

--- src/main/java/com/varausjarjestelma/malli/Ominaisuus.java
+++ src/main/java/com/varausjarjestelma/malli/Ominaisuus.java
@@ -2,6 +2,14 @@ package com.varausjarjestelma.malli;
 
 import javax.persistence.*;
 
+/**
+ * A hibernate entity used to map a row
+ * in the attribute (Ominaisuus) table in the
+ * database.
+ * 
+ * @author V. Ahlstén
+ *
+ */
 @Entity
 @Table(name="Ominaisuus")
 public class Ominaisuus {
@@ -16,6 +24,12 @@ public class Ominaisuus {
 	@Column(name="kuvaus", length=500)
 	private String kuvaus;
 	
+	/**
+	 * Returns the value in the member
+	 * variable id.
+	 * 
+	 * @return id
+	 */
 	public int getID() {
 		return id;
 	}
@@ -24,18 +38,42 @@ public class Ominaisuus {
 		this.id = id;
 	}
 
+	/**
+	 * Returns the value in the member
+	 * variable nimi.
+	 * 
+	 * @return nimi
+	 */
 	public String getNimi() {
 		return nimi;
 	}
 
+	/**
+	 * Sets a new value to the member
+	 * variable nimi.
+	 * 
+	 * @param nimi
+	 */
 	public void setNimi(String nimi) {
 		this.nimi = nimi;
 	}
 
+	/**
+	 * Returns the value in the member
+	 * variable kuvaus.
+	 * 
+	 * @return kuvaus
+	 */
 	public String getKuvaus() {
 		return kuvaus;
 	}
 
+	/**
+	 * Sets a new value to the member
+	 * variable kuvaus.
+	 * 
+	 * @param kuvaus
+	 */
 	public void setKuvaus(String kuvaus) {
 		this.kuvaus = kuvaus;
 	}
