7d6175462e9944342825e78edfb67a26442bd96c
Sebastian Sarviala <sebastian.sarviala@metropolia.fi>
2020-12-10 16:56:44 +0200

    EmailMain poistettu, SimppeliMaili Javadoc-kommentit

--- src/main/java/com/varausjarjestelma/sähköposti/SimppeliMaili.java
+++ src/main/java/com/varausjarjestelma/sähköposti/SimppeliMaili.java
@@ -4,74 +4,76 @@ package com.varausjarjestelma.sähköposti;
 import java.security.Timestamp;
 import java.util.*;
 import java.util.logging.Level;
-
-import javax.mail.*;
-import javax.mail.internet.*;
-
-
-import javax.activation.*;
-
 import java.util.logging.Logger;
+
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import com.varausjarjestelma.logiikka.Varausmanageri;
 
+/**
+ * A class for sending email messages.
+ * 
+ * @author E. Niemi
+ *
+ */
 public class SimppeliMaili {
-	
+
 	String varatunTilanNimi;
 	Timestamp aloitusaika;
 	Timestamp lopetusaika;
-	
 
-   public static void sendMail(String recipient) throws MessagingException {    
-      System.out.println("valmistaudutaan emailin lähettämiseen..");
-	   Properties properties = new Properties();
-	   properties.put("mail.smtp.auth", true);
-	   properties.put("mail.smtp.starttls.enalble", true);
-	   properties.put("mail.smtp.host", "smtp.gmail.com");
-	   properties.put("mail.smtp.port", "587");
-	   properties.put("mail.smtp.starttls.enable", "true");
-	   
-	   //lähettäjäosoite ja salasana
-	   final String myAccountEmail = "reservicefinland@gmail.com";
-	   final String password = "team7team7";
-	   
-	   Session session = Session.getInstance(properties, new Authenticator() {
-		   
-		   protected PasswordAuthentication getPasswordAuthentication() {
-			   return new PasswordAuthentication(myAccountEmail, password);
-		   }
-	   });
-	   Message message = prepareMessage(session, myAccountEmail, recipient);
-	   Transport.send(message);
-	   System.out.println("viesti lähetetty!");
-	   
-   }
+	/**
+	 * Sends a message to the email address
+	 * passed as an argument.
+	 * 
+	 * @param recipient
+	 * @throws MessagingException
+	 */
+	public static void sendMail(String recipient) throws MessagingException {
+		System.out.println("valmistaudutaan emailin lähettämiseen..");
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", true);
+		properties.put("mail.smtp.starttls.enalble", true);
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.starttls.enable", "true");
 
-private static Message prepareMessage(Session session, String myAccountEmail, String recipient) {
-	try {
-	Message message = new MimeMessage(session);
-	message.setFrom(new InternetAddress(myAccountEmail));
-	message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-	message.setSubject("Reserviceltä viestiä!!");
-	String osaviestiä = new Varausmanageri().varaustiedotMailiin();
-	message.setText("Reserviceltä terve, " + osaviestiä + " Kiitos!");
-	return message;
-	} catch(Exception ex) {
-		Logger.getLogger(SimppeliMaili.class.getName()).log(Level.SEVERE, null, ex);
+		// lähettäjäosoite ja salasana
+		final String myAccountEmail = "reservicefinland@gmail.com";
+		final String password = "team7team7";
+
+		Session session = Session.getInstance(properties, new Authenticator() {
+
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(myAccountEmail, password);
+			}
+		});
+		Message message = prepareMessage(session, myAccountEmail, recipient);
+		Transport.send(message);
+		System.out.println("viesti lähetetty!");
+
 	}
-	return null;
-}
-   
-   
-   
-}
 
+	private static Message prepareMessage(Session session, String myAccountEmail, String recipient) {
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(myAccountEmail));
+			message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+			message.setSubject("Reserviceltä viestiä!!");
+			String osaviestiä = new Varausmanageri().varaustiedotMailiin();
+			message.setText("Reserviceltä terve, " + osaviestiä + " Kiitos!");
+			return message;
+		} catch (Exception ex) {
+			Logger.getLogger(SimppeliMaili.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return null;
+	}
+
+}
