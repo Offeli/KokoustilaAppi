2a552481ebda70b5f32ee0464e78ce0054b99ccc
Sebastian Sarviala <sebastian.sarviala@metropolia.fi>
2020-12-10 15:45:09 +0200

    Javadoc-kommentit luokalle TilanOminaisuus

--- src/main/java/com/varausjarjestelma/malli/TilanOminaisuus.java
+++ src/main/java/com/varausjarjestelma/malli/TilanOminaisuus.java
@@ -1,6 +1,14 @@
 package com.varausjarjestelma.malli;
 import javax.persistence.*;
 
+/**
+ * A hibernate entity used to map a row
+ * in the room-attribute connector 
+ * (TilanOminaisuus) table in the database.
+ * 
+ * @author V. Ahlstén
+ *
+ */
 @Entity
 @Table(name="TilanOminaisuus")
 public class TilanOminaisuus {
@@ -20,34 +28,82 @@ public class TilanOminaisuus {
 	@Column(name="lisatietoa", length=500)
 	private String lisatietoa;
 	
+	/**
+	 * Returns the value in the member
+	 * variable id.
+	 * 
+	 * @return id
+	 */
 	public int getID() {
 		return id;
 	}
 
+	/**
+	 * Sets a new value to the member
+	 * variable id.
+	 * 
+	 * @param id
+	 */
 	public void setID(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Returns the value in the member
+	 * variable tila.
+	 * 
+	 * @return tila
+	 */
 	public Tila getTila() {
 		return tila;
 	}
 
+	/**
+	 * Sets a new value to the member
+	 * variable tila.
+	 * 
+	 * @param tila
+	 */
 	public void setTila(Tila tila) {
 		this.tila = tila;
 	}
 	
+	/**
+	 * Returns the value in the member
+	 * variable ominaisuus.
+	 * 
+	 * @return ominaisuus
+	 */
 	public Ominaisuus getOminaisuus() {
 		return ominaisuus;
 	}
 
+	/**
+	 * Sets a new value to the member
+	 * variable ominaisuus.
+	 * 
+	 * @param ominaisuus
+	 */
 	public void setOminaisuus(Ominaisuus ominaisuus) {
 		this.ominaisuus = ominaisuus;
 	}
 
+	/**
+	 * Returns the value in the member
+	 * variable lisatietoa.
+	 * 
+	 * @return lisatietoa
+	 */
 	public String getLisatiedot() {
 		return lisatietoa;
 	}
 
+	/**
+	 * Sets a new value to the member
+	 * variable lisatietoa.
+	 * 
+	 * @param lisatietoa
+	 */
 	public void setLisatiedot(String lisatietoa) {
 		this.lisatietoa = lisatietoa;
 	}
